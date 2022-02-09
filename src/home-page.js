8ceb5512b2a02eda37cff384c4d378f6bf15d5b7
Luke Preston <luke.preston@trailapp.com>
2022-02-09 16:36:05 +0000

    changed the home page

--- src/home-page.js
+++ src/home-page.js
@@ -4,29 +4,7 @@ const Home = () => (
     <div className='content mt-3'>
         <h1 className='title'>Dream Dice</h1>
         <p>
-            Hello, if you  have come here it is probably because you are either a player, refered to by a player or perhaps the DM (me). In any case I hope you find what you are looking for.
-        </p>
-        <p>
-            Instead of keeping everything hidden in my docs from my players I thought it would be more fun that they can see everything I have been keeping a note of. I wanted to keep it like a story or game content and make it more interesting.
-        </p>
-        <h2 className='subtitle'>Library</h2>
-        <p>
-            In here you will find a library of a write-up of the campaign I have been running. It started out as a recap then turned into a short novel recounting the interesting events that occured from the view of someone in their world.
-        </p>
-        <p>
-            I will also aime to provide links to books and source materials from the campaign so that any player who would like to be a DM, or is interested in the work behind the campaign can do.
-        </p>
-        <h2 className='subtitle'>Factions</h2>
-        <p>
-            I have always liked the idea of factions and here is the list of factions. The manifesto includes details to help you role play as members of those factions. There is also a list of jobs which I intend to keep up-to-date as I have been doing with the story, this way players can reference jobs they have completed or ones they have yet to complete.
-        </p>
-        <h2 className='subtitle'>Players</h2>
-        <p>
-            I have also put in a summary of each player, again keeping this up to date where possible. I wanted to keep a list of notable things from each player which I hope will aide everyone in role playing with one another as well as notes for me to refer to. I will encourage the players to look at these and help me keep them accurate and up-to-date.
-        </p>
-        <h2 className='subtitle'>Game Master</h2>
-        <p>
-            A place for tools I have created for myself, or things like bookmarks to use later. I have also put a credits section in order to thank anyone who I have taken content from.
+            Hey there, this is a website I use to be able to share info with players. I beleive that my players should have the same infomation that I have (without the spoilers). This is my attempt at making easier for them as well as a place for me to go to when I need to see my notes!
         </p>
     </div>
 )
