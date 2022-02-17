ee9d1d4dc53e088e42dfb8c0c33e2023713e805c
Luke Preston <luke.preston@trailapp.com>
2022-02-17 23:12:13 +0000

    searching now uses url

--- src/characters-page.js
+++ src/characters-page.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useNavigate, useLocation } from "react-router-dom";
+import qs from 'qs'
 import Card from './card'
 import players from './players.json'
 
 const CharactersPage = ({ game }) => {
-    const [term, setTerm] = useState('')
+    const navigate = useNavigate()
+    const {pathname, search} = useLocation()
+    const query = qs.parse(search, {ignoreQueryPrefix: true})
+    const term = query.term || ''
+
+    const searchTerm = term.toLowerCase().replace(/\W*/g, '')
 
     return (
         <div className='characters'>
@@ -12,13 +19,16 @@ const CharactersPage = ({ game }) => {
                     className="input mb-4"
                     type="text"
                     placeholder="Search name"
-                    onChange={({ target: { value } }) => setTerm(value.toLowerCase().replace(/\W*/g, ''))}
+                    value={term}
+                    onChange={({ target: { value } }) => {
+                        navigate(`${pathname}?term=${value}`)
+                    }}
                 />
                 {
                     players[game]
                         .filter(({ title }) => title.toLowerCase()
                             .replace(/\W*/g, '')
-                            .includes(term)
+                            .includes(searchTerm)
                         )
                         .sort(({title: left}, {title: right}) => {
                             if (left < right) return -1
--- src/notes-page.js
+++ src/notes-page.js
@@ -1,23 +1,34 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useNavigate, useLocation } from "react-router-dom";
+import qs from 'qs'
 import Card from './card'
 import notes from './notes.json'
 
 const NotesPage = ({ game }) => {
-    const [term, setTerm] = useState('')
+    const navigate = useNavigate()
+    const {pathname, search} = useLocation()
+    const query = qs.parse(search, {ignoreQueryPrefix: true})
+    const term = query.term || ''
+
+    const searchTerm = term.toLowerCase().replace(/\W*/g, '')
+
     return <div className='notes'>
         <div>
             <input
                 className="input mb-4"
                 type="text"
                 placeholder="Search content"
-                onChange={({ target: { value } }) => setTerm(value.toLowerCase().replace(/\W*/g, ''))}
+                value={term}
+                onChange={({ target: { value } }) => {
+                    navigate(`${pathname}?term=${value}`)
+                }}
             />
             {notes[game]
                 .filter(({ title, description, notes }) => {
-                    if (term === '') return true
-                    if (title.toLowerCase().replace(/\W*/g, '').includes(term)) return true
-                    if (description.toLowerCase().replace(/\W*/g, '').includes(term)) return true
-                    if (notes.join(' ').toLowerCase().replace(/\W*/g, '').includes(term)) return true
+                    if (searchTerm === '') return true
+                    if (title.toLowerCase().replace(/\W*/g, '').includes(searchTerm)) return true
+                    if (description.toLowerCase().replace(/\W*/g, '').includes(searchTerm)) return true
+                    if (notes.join(' ').toLowerCase().replace(/\W*/g, '').includes(searchTerm)) return true
                     return false
 
                 })
