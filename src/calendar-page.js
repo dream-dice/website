1bb683f1c7a4fa1efde28f97a2a009987ac17d82
Luke Preston <lukejpreston@gmail.com>
2023-03-06 18:30:04 +0000

    remove next

--- src/calendar-page.js
+++ src/calendar-page.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Moon } from 'lunarphase-js';
 import Card from './card'
 import calendar from './calendar.json'
-import { createEvent } from 'ics'
-import Discord from './discord';
 
 const timeOfDay = (time) => {
     const hour = time.getHours()
@@ -55,67 +53,11 @@ const Events = ({ events }) => (
     </Card>
 )
 
-const NextSession = ({ title, description, date, discord }) => (
-    <div className='content'>
-        <h3 className='subtitle mb-1'>{title}</h3>
-        <p>{description}</p>
-        <div className='buttons'>
-            <button
-                className='button is-link'
-                onClick={() => {
-                    date = new Date(date)
-
-                    const event = {
-                        start: [date.getUTCFullYear(), date.getUTCMonth() + 1, date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes()],
-                        duration: { hours: 2, minutes: 30 },
-                        title: title,
-                        description: description,
-                        url: 'http://foundry.blankstring.com',
-                        categories: ['game', 'dnd'],
-                        organizer: { name: 'Luke Preston', email: 'lukejpreston@gmail.com' }
-                    }
-
-                    createEvent(event, (error, value) => {
-                        var element = document.createElement('a');
-                        element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(value)}`);
-                        element.setAttribute('download', `${title}.ics`);
-                        element.style.display = 'none';
-                        document.body.appendChild(element);
-                        element.click();
-                        document.body.removeChild(element);
-                    })
-                }}
-            >
-                📆 Download event
-            </button>
-            <a
-                disabled={typeof discord === 'undefined'}
-                className='button is-text'
-                href={discord || undefined}
-                rel='noopener noreferrer'
-                target='_blank'
-            >
-                <span className='icon mr-1'>
-                    <Discord />
-                </span>
-                Discord link
-            </a>
-        </div>
-    </div>
-)
-
 const CalendarPage = ({ game }) => {
-    const { events, nextSession, time, weather } = calendar[game]
+    const { events, time, weather } = calendar[game]
 
     return (
         <div>
-            <Card
-                title='Next Session'
-                subtitle={new Date(nextSession.date).toLocaleString('en-GB', { timeZone: 'UTC' })}
-                isOpen
-            >
-                <NextSession {...nextSession} />
-            </Card>
             <Card
                 title='Today'
                 subtitle={new Date(time).toLocaleString('en-GB', { timeZone: 'UTC' })}
