5f64556172f45bd694a1c91ec6a7e0b5488f5811
Luke Preston <lukejpreston@gmail.com>
2023-04-18 21:29:33 +0100

    experiment image

--- scripts/compress-maps.sh
+++ scripts/compress-maps.sh
@@ -2,17 +2,19 @@
 
 if [ -n "$(ls -A temp/maps)" ]; then
     for i in temp/maps/*; do
+        echo $i
         if test -f "/home/luke/Projects/website/public/hotlink-ok/maps/$(basename $i)"; then
             echo "/home/luke/Projects/website/public/hotlink-ok/maps/$(basename $i) exists."
         elif [[ $i != *.jpg ]]; then
             mv $i "/home/luke/Projects/website/public/hotlink-ok/maps/$(basename $i)"
         elif [[ $i == *.jpg ]]; then
+            echo /home/luke/Projects/website/temp/maps/compressed
+            compress --source $i --destination temp/compressed
+            infile="/home/luke/Projects/website/temp/compressed/$(basename $i)"
             file="/home/luke/Projects/website/public/hotlink-ok/maps/$(basename $i)"
-            compress --source $i --destination temp/maps/compressed
-            infile="/home/luke/Projects/website/temp/maps/compressed/$(basename $i)"
             cwebp -q 60 $infile -o "${file%.jpg}.webp"
-            mv "${file%.jpg}.webp" /home/luke/Projects/website/public/hotlink-ok/maps/${file%.jpg}.webp"
-            rm $i $infile "${file%.jpg}.webp"
+            rm $i $infile
+            echo "Created ${file%.jpg}.webp"
         fi
     done
 else
